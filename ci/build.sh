426019ae459f9088583c5f64f2394a5584377bd7
Ben Hale <bhale@pivotal.io>
2018-11-01 13:14:24 -0700

    Go Module Caching
    This change updates the CI scripts to properly cache Go Module downloads.
    
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- ci/build.sh
+++ ci/build.sh
@@ -2,7 +2,10 @@
 
 set -euo pipefail
 
-GOCACHE="$PWD/go-build"
+if [[ -d $PWD/go-module-cache && ! -d $GOPATH/pkg/mod ]]; then
+  mkdir -p $GOPATH/pkg
+  ln -s $PWD/go-module-cache $GOPATH/pkg/mod
+fi
 
-GOOS=linux    GOARCH=amd64 go build -i -ldflags='-s -w' -o bin/build build/main.go
-GOOS=linux    GOARCH=amd64 go build -i -ldflags='-s -w' -o bin/detect detect/main.go
+GOOS=linux GOARCH=amd64 go build -i -ldflags='-s -w' -o bin/build build/main.go
+GOOS=linux GOARCH=amd64 go build -i -ldflags='-s -w' -o bin/detect detect/main.go
