bf6f4c40a3c520de7767f32e5824cf51b3f93a6b
Eric Bottard <ebottard@pivotal.io>
2018-11-22 15:34:28 +0100

    More shameful fixes

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,5 +4,5 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-source `dirname "${BASH_SOURCE[0]}"`/deploy.sh
+source `dirname "${BASH_SOURCE[0]}"`/upload-to-gcs.sh
 source `dirname "${BASH_SOURCE[0]}"`/trigger-builder-build.sh
