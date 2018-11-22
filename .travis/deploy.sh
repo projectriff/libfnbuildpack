f0909ca4abd73e00d7f97c9c4cdff6b2f1040fdd
Eric Bottard <ebottard@pivotal.io>
2018-11-22 15:28:55 +0100

    Fix relative paths

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,5 +4,5 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-./deploy.sh
-./trigger-builder-build.sh
+source `dirname "${BASH_SOURCE[0]}"`/deploy.sh
+source `dirname "${BASH_SOURCE[0]}"`/trigger-builder-build.sh
