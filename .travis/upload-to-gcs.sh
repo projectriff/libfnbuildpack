59452193afa0f2e66ea51b618c727cb77b144a2f
Eric Bottard <ebottard@pivotal.io>
2018-12-11 12:12:11 +0100

    Upload as latest.tgz only on master

--- .travis/upload-to-gcs.sh
+++ .travis/upload-to-gcs.sh
@@ -4,7 +4,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-gcloud auth activate-service-account --key-file <(echo $GCLOUD_CLIENT_SECRET | base64 --decode)
+gcloud auth activate-service-account --key-file <(echo ${GCLOUD_CLIENT_SECRET} | base64 --decode)
 
 gsutil cp -a public-read scratch/io/projectriff/riff/io.projectriff.riff/*/*.tgz gs://projectriff/riff-buildpack/
-gsutil cp -a public-read scratch/io/projectriff/riff/io.projectriff.riff/*/*.tgz gs://projectriff/riff-buildpack/latest.tgz
+if [ "${TRAVIS_BRANCH}" = master ] ; then
+    gsutil cp -a public-read scratch/io/projectriff/riff/io.projectriff.riff/*/*.tgz gs://projectriff/riff-buildpack/latest.tgz
+fi
